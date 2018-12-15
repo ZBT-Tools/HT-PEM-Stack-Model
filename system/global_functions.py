9ec47efe730d2b60b2402547e25458936578dec6
Simon15043 <s.jurkschat@web.de>
2018-12-15 20:29:14 +0100

    -cleared the file global_functions.py -added comments to the functions of the file global_functions.py

--- system/global_functions.py
+++ system/global_functions.py
@@ -4,30 +4,51 @@ import os
 
 
 def dw(t):
+    """
+    Calculates the free water content diffusion coefficient in the membrane.
+    """
+
     return 2.1e-7 * np.exp(-2436. / t)
 
 
-def d_pos(t):
-    return 1.6e-8 * np.exp(-1683. / t)
-
-
 def to_array(var, m, n):
+    """
+    Changes a sorted 1-d-array to an sorted 2-d-array.
+    """
+
     return np.reshape(var.flatten(order='C'), (m, n))
 
 
 def calc_dif(vec):
+    """
+    Calculates the difference between the i+1 and i position of an 1-d-array.
+    """
+
     return vec[:-1] - vec[1:]
 
 
 def calc_rho(p, r, t):
+    """
+    Calculates the density of an ideal gas.
+    """
+
     return p / (r * t)
 
 
 def calc_Re(roh, v, d, visc):
+    """"
+    Calculates the reynolds number of an given fluid.
+    """
+
     return roh * v * d / visc
 
 
 def calc_fan_fri_fac(re):
+    """
+    Calculates the fanning friction factor between a wall
+    and a fluid for the laminar and turbulent case.
+    """
+
     f = np.full(len(re), 0.)
     for q, item in enumerate(re):
         if 0. <= re[q] <= 2100.:
@@ -38,12 +59,21 @@ def calc_fan_fri_fac(re):
 
 
 def calc_head_p_drop(roh, v1, v2, f, kf, le, dh):
+    """
+    Calculates the pressure drop at an defined t-junction,
+    according to (Koh, 2003).
+    """
+
     a = (v1**2. - v2**2.) * .5
     b = v2**2. * (2. * f * le / dh + kf * .5)
     return roh * (a + b)
 
 
 def calc_visc_mix(visc, mol_f, mol_m):
+    """
+    Calculates the mixture viscosity of a gas acording to Herning ad Zipperer.
+    """
+
     visc = np.array(visc).transpose()
     mol_f = np.array(mol_f).transpose()
     visc_mix = []
@@ -55,6 +85,9 @@ def calc_visc_mix(visc, mol_f, mol_m):
 
 
 def calc_psi(visc, mol_w):
+    """
+    Calculates the wilke coefficients for each species combination of a gas.
+    """
     psi = []
     for q in range(len(visc)):
         for w in range(len(visc)):
@@ -66,6 +99,11 @@ def calc_psi(visc, mol_w):
 
 
 def calc_lambda_mix(lambdax, mol_f, visc, mol_w):
+    """
+    Calculates the heat conductivity of a gas mixture,
+    according to Wilkes equation.
+    """
+
     mol_f[1:] = np.minimum(1.e-20, mol_f[1:])
     psi = calc_psi(visc, mol_w)
     counter = 0
@@ -79,26 +117,40 @@ def calc_lambda_mix(lambdax, mol_f, visc, mol_w):
         outcome = outcome + a / b
     return outcome
 
-lama = []
-mol_f = []
-visc = []
-mol_w = []
-
 
 def calc_elements_1_d(node_vec):
+    """
+    Calculates an element 1-d-array from a node 1-d-array.
+    """
+
     return np.array((node_vec[:-1] + node_vec[1:])) * .5
 
 
 def calc_elements_2d(node_mat):
+    """
+    Calculates an element 2-d-array from a node 2-d-array.
+    """
+
     return np.array((node_mat[:, :-1] + node_mat[:, 1:])) * .5
 
 
 def calc_nodes_2_d(ele_mat):
+    """
+    Calculates an node 2-d-array from an element 2-d-array,
+    uses the [:, 1], [:, -2] entries of the calculated node 2-d-array
+    to fill the first als last row of the node 2-d-array.
+    """
+
     mat = np.array((ele_mat[:, :-1] + ele_mat[:, 1:])) * .5
     return np.hstack([mat[:, [0]], mat, mat[:, [-1]]])
 
 
 def i_e_polate_nodes_2_d(ele_vec):
+    """
+    Calculates an node 2-d-array from an element 2-d-array,
+    and interpolates the first and last row of the node 2-d-array.
+    """
+
     re_array = np.array((ele_vec[:, :-1] + ele_vec[:, 1:])) * 0.5
     first_node = np.array([2. * re_array[:, 0] - re_array[:, 1]])
     last_node = np.array([2. * re_array[:, -1] - re_array[:, -2]])
@@ -106,48 +158,22 @@ def i_e_polate_nodes_2_d(ele_vec):
 
 
 def calc_nodes_1_d(ele_vec):
+    """
+    Calculates an node 1-d-array from an element 1-d-array,
+    uses the [:, 1], [:, -2] entries of the calculated node 1-d-array
+    to fill the first als last row of the node 1-d-array.
+    """
+
     vec = np.array((ele_vec[:-1] + ele_vec[1:])) * .5
     return np.hstack([vec[0], vec, vec[-1]])
 
 
-def calc_ie_dx(vec1, vec2):
-    vec1 = np.array(vec1)
-    vec2 = np.array(vec2)
-    diff1 = vec1[1:] - vec1[:-1]
-    diff2 = vec2[1:] - vec2[:-1]
-    res_vec = diff1 / diff2
-    res_vec = (res_vec[:-1] + res_vec[1:]) * .5
-    return np.concatenate(([2. * res_vec[0] - res_vec[1]],
-                           res_vec,
-                           [2. * res_vec[-1] - res_vec[-2]]))
-
-
 def calc_fluid_water_enthalpy(t):
+    """
+    Calculates the enthalpy of fluid water by a given temperature.
+    """
+
     return (t-273.15) * 4182.
-
-
-def stack_mat_bc(mat, times):
-    array_stack = []
-    for q in range(times):
-        array_stack.append(mat)
-    array_stack[0] = 0.5 * array_stack[0]
-    array_stack[-1] = 0.5 * array_stack[-1]
-    return np.array(array_stack)
-
-
-def stack_mat(mat, times):
-    array_stack = []
-    for q in range(times):
-        array_stack.append(mat)
-    return np.array(array_stack)
-
-
-def change_concrete_4d_too_3d(array):
-    list = []
-    for q in range(len(array)):
-        for w, item in enumerate(array[q]):
-            list.append(item)
-    return np.array(list)
 
 
 def output(y_values, y_label, x_label, y_scale, color,
@@ -199,4 +225,10 @@ def output_x(y_values, x_values, y_label, x_label,
 
 
 def calc_fluid_temp_out(temp_in, temp_wall, g, k):
+    """
+    Calculates the linearised fluid outlet temperature
+    of an element by the wall temperature and the fluid inlet temperature.
+    The function is limited to cases with g > 0.5 * k.
+    """
+
     return (temp_in * (g - .5 * k) + temp_wall * k) / (g + k * .5)
