2642246bd658491ca60284266c6f323eec415dab
lufire <lukas.feierabend@gmail.com>
2020-01-09 01:48:08 +0100

    subclassing ParallelFlowCircuit

--- system/flow_circuit.py
+++ system/flow_circuit.py
@@ -4,9 +4,21 @@ import system.global_functions as g_func
 import copy as copy
 import system.channel as chl
 from system.output_object import OutputObject
+import system.fluid2 as fluids
+from abc import ABC
 
-
-class ParallelFlowCircuit(OutputObject):
+class ParallelFlowCircuit(ABC, OutputObject):
+    def __new__(cls, dict_flow_circuit, manifolds, channels,
+                channel_multiplier=1.0):
+        fluid = manifolds[0].fluid
+        if type(fluid) is fluids.IncompressibleFluid:
+            return super(ParallelFlowCircuit, cls).__new__(HomogeneousFluidFlowCircuit)
+        elif type(fluid) is fluids.GasMixture:
+            return super(ParallelFlowCircuit, cls).__new__(GasMixtureFlowCircuit)
+        elif type(fluid) is fluids.TwoPhaseMixture:
+            return super(ParallelFlowCircuit, cls).__new__(TwoPhaseMixtureFlowCircuit)
+        else:
+            raise NotImplementedError
 
     def __init__(self, dict_flow_circuit, manifolds, channels,
                  channel_multiplier=1.0):
@@ -15,15 +27,14 @@ class ParallelFlowCircuit(OutputObject):
         assert isinstance(dict_flow_circuit, dict)
         assert isinstance(manifolds, (list, tuple))
         assert isinstance(channels,  (list, tuple))
+        err_message = 'manifolds must be tuple or list with two objects of ' \
+                      'class Channel'
         if len(manifolds) != 2:
-            raise ValueError('manifolds must be tuple or list with two objects '
-                             'of class Channel')
+            raise ValueError(err_message)
         elif not isinstance(manifolds[0], chl.Channel):
-            raise TypeError('manifolds must be tuple or list with two objects '
-                            'of class Channel')
+            raise TypeError(err_message)
         if not isinstance(channels[0], chl.Channel):
-            raise TypeError('channels must be tuple or list with objects '
-                            'of class Channel')
+            raise TypeError(err_message)
         self.manifolds = manifolds
         self.channels = channels
         self.n_channels = len(self.channels)
@@ -36,6 +47,20 @@ class ParallelFlowCircuit(OutputObject):
         self.vol_flow_in = self.manifolds[0].vol_flow[0]
         self.channel_vol_flow = \
             self.alpha * self.vol_flow_in / self.n_channels
+
+class HomogeneousFluidFlowCircuit(ParallelFlowCircuit, OutputObject):
+    def __init__(self, dict_flow_circuit, manifolds, channels,
+                 channel_multiplier=1.0):
+        super().__init__(dict_flow_circuit, manifolds, channels,
+                         channel_multiplier)
+
+class GasMixtureFlowCircuit(ParallelFlowCircuit, OutputObject):
+
+    def __init__(self, dict_flow_circuit, manifolds, channels,
+                 channel_multiplier=1.0):
+        super().__init__(dict_flow_circuit, manifolds, channels,
+                         channel_multiplier)
+
         # self.kf = dict_flow_circuit['kf']
         self.channel_length = \
             np.asarray([channel.length for channel in channels])
@@ -78,7 +103,7 @@ class ParallelFlowCircuit(OutputObject):
         self.cell_mol_flow_old = np.full(self.n_channels, 0.)
         self.criteria = 0.
 
-    def update(self, vol_flow_in=None):
+    def iniltialize(self, vol_flow_in=None):
         """
         Update the flow circuit
         """
@@ -86,7 +111,9 @@ class ParallelFlowCircuit(OutputObject):
             self.vol_flow_in = vol_flow_in
         self.channel_vol_flow = \
             self.alpha * self.vol_flow_in / self.n_channels
-        dmol_inlet_header = self.chann
+        channel_mol_flows = np.array([channel.mol_flow[:, 0]
+                                      for channel in self.channels]).transpose()
+        dmol_inlet_header = self.channel_vol_flow * self.manifolds[0].fluid.mw
 
     def calc_header_temperature(self):
         """
@@ -148,3 +175,33 @@ class ParallelFlowCircuit(OutputObject):
         self.criteria = \
             np.sum(((self.cell_mol_flow[0] - self.cell_mol_flow_old)
                     / self.cell_mol_flow_old)**2)
+
+class TwoPhaseMixtureFlowCircuit(GasMixtureFlowCircuit, OutputObject):
+    def __init__(self, dict_flow_circuit, manifolds, channels,
+                 channel_multiplier=1.0):
+        super().__init__(dict_flow_circuit, manifolds, channels,
+                         channel_multiplier)
+
+
+def flow_circuit_factory(dict_fluid, dict_channel, dict_in_manifold,
+                        dict_out_manifold, n_channels, channel_multiplier=1.0):
+    nx = g_par.dict_case['nodes']
+    fluid = \
+        [fluids.fluid_factory(nx, dict_fluid['fluid_name'],
+                              species_dict=dict_fluid['fluid_components'],
+                              mole_fractions=
+                              dict_fluid['inlet_composition'])
+         for i in range(n_channels)]
+    channels = [chl.Channel(dict_channel, fluid[i]) for i in range(n_channels)]
+    fluid = \
+        [fluids.fluid_factory(n_channels, dict_fluid['fluid_name'],
+                              species_dict=dict_fluid['fluid_components'],
+                              mole_fractions=
+                              dict_fluid['inlet_composition'])
+         for i in range(2)]
+    manifolds = [chl.Channel(dict_in_manifold, fluid[0]),
+                 chl.Channel(dict_out_manifold, fluid[1])]
+
+    flow_circuit_dict = {'name': 'Flow Circuit'}
+    return ParallelFlowCircuit(flow_circuit_dict, manifolds, channels,
+                               channel_multiplier= hannel_multiplier)
