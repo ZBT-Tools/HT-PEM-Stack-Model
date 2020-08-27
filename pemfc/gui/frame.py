a6797aa9548af29d7ebf6de131c3bd14e47d3e28
lufire <lukas.feierabend@gmail.com>
2020-08-28 00:27:10 +0200

    editing grid settings

--- pemfc/gui/frame.py
+++ pemfc/gui/frame.py
@@ -25,24 +25,27 @@ class BaseFrame(tk.Frame):
 
     def __init__(self, master, widget_set_dicts: list = None, **kwargs):
         title = kwargs.pop('title', None)
+        self.name = None
         if title is not None:
             self.name = title.lower()
-        else:
-            self.name = title
         super().__init__(master, name=self.name, **kwargs)
-        self.grid()
+        # self.grid()
         self.widget_factory = ws.WidgetSetFactory()
-
+        # self.widget_sets = []
         if widget_set_dicts is not None:
             self.widget_sets = [self.widget_factory.create_set(self, **ws_dict)
                                 for ws_dict in widget_set_dicts]
+            # for ws_dict in widget_set_dicts:
+            #     self.widget_sets.append(self.widget_factory.create_set(self, **ws_dict))
+            #     print(title, self.grid_size(), 'BaseFrame')
         self.title = None
         print(title, self.grid_size(), 'BaseFrame')
         print('master', master)
         if title is not None:
             self.title = self.set_title(title)
 
-    def set_title(self, text, font=None, bold=True):
+    def set_title(self, text, row=0, column=0, font=None, bold=True,
+                  **kwargs):
         if text is not None:
             columns = self.grid_size()[0]
             if font is None:
@@ -55,9 +58,19 @@ class BaseFrame(tk.Frame):
             else:
                 columnspan = columns
             title = tk.Label(self, text=text, font=font)
-            title.grid(row=0, column=0, columnspan=columnspan,
-                       padx=self.PADX, pady=self.PADY)
+            title.grid(row=row, column=column, columnspan=columnspan,
+                       padx=kwargs.get('padx', self.PADX),
+                       pady=kwargs.get('pady', self.PADY), **kwargs)
             return title
+
+    def set_grid(self, grid_list=None, **kwargs):
+        if grid_list is None:
+            for i, widget_set in enumerate(self.widget_sets):
+                kwargs.pop('row', None)
+                kwargs.pop('column', None)
+                row = i + 1
+                column = 0
+                widget_set.set_grid(row=row, column=column, **kwargs)
 
 
 class MainFrame(BaseFrame):
@@ -71,12 +84,22 @@ class MainFrame(BaseFrame):
         if sub_frame_dicts is not None:
             for i, frame_dict in enumerate(sub_frame_dicts):
                 sub_frame = self.frame_factory.create_frame(self, **frame_dict)
-                sub_frame.grid(row=i)
+                # sub_frame.grid(row=i)
                 self.sub_frames.append(sub_frame)
         print(title, self.grid_size(), 'MainFrame')
         print('master', master)
+        self.set_grid()
         if show_title:
             self.set_title(title)
+
+    def set_grid(self, grid_list=None, **kwargs):
+        if grid_list is None:
+            for i, sub_frame in enumerate(self.sub_frames):
+                kwargs.pop('row', None)
+                kwargs.pop('column', None)
+                row = i + 1
+                column = 0
+                sub_frame.set_grid(row=row, column=column, **kwargs)
 
 
 
--- pemfc/gui/widget_set.py
+++ pemfc/gui/widget_set.py
@@ -1,6 +1,6 @@
 # global imports
 import tkinter as tk
-from abc import ABC
+from abc import ABC, abstractmethod
 
 # local imports
 from pemfc.src import global_functions as gf
@@ -35,17 +35,31 @@ class WidgetSet(ABC):
         self.padx = kwargs.pop('padx', self.PADX)
         self.pady = kwargs.pop('pady', self.PADY)
         self.column = kwargs.pop('column', 0)
-        self.row = kwargs.pop('row', frame.grid_size()[1] + 1)
+        self.row = kwargs.pop('row', frame.grid_size()[1])
         kwargs['text'] = label
         sticky = kwargs.pop('sticky', 'W')
         self.label = tk.Label(frame, **kwargs)
-        self.label.grid(row=self.row, column=self.column, padx=self.padx,
-                        pady=self.pady, sticky=sticky)
+        # self.label.grid(row=self.row, column=self.column, padx=self.padx,
+        #                 pady=self.pady, sticky=kwargs.pop('sticky', 'W'))
+
+    @abstractmethod
+    def set_grid(self, row=None, column=None, **kwargs):
+        if row is None:
+            row = self.row
+        if column is None:
+            column = self.column
+        self.label.grid(row=row, column=column,
+                        padx=kwargs.get('padx', self.PADX),
+                        pady=kwargs.get('pady', self.PADY),
+                        sticky=kwargs.pop('sticky', 'W'), **kwargs)
 
 
 class Label(WidgetSet):
     def __init__(self, frame, label, **kwargs):
         super().__init__(frame, label, **kwargs)
+
+    def set_grid(self, row=None, column=None, **kwargs):
+        super().set_grid(row=row, column=column, **kwargs)
 
 
 class MultiEntrySet(WidgetSet):
@@ -57,12 +71,20 @@ class MultiEntrySet(WidgetSet):
         self.entries = []
         for i in range(number):
             entry = tk.Entry(frame, justify='right')
-            entry.grid(row=self.row, column=self.column + 1 + i,
-                       padx=self.padx, pady=self.pady)
+            # entry.grid(row=self.row, column=self.column + 1 + i,
+            #            padx=self.padx, pady=self.pady)
             entry.delete(0, -1)
             if value is not None:
                 entry.insert(0, value[i])
             self.entries.append(entry)
+
+    def set_grid(self, row=None, column=None, **kwargs):
+        super().set_grid(row, column, **kwargs)
+        for i, entry in enumerate(self.entries):
+            column += 1
+            entry.grid(row=row, column=column,
+                       padx=kwargs.get('padx', self.PADX),
+                       pady=kwargs.get('pady', self.PADY), **kwargs)
 
 
 class DimensionedEntrySet(MultiEntrySet):
@@ -71,9 +93,16 @@ class DimensionedEntrySet(MultiEntrySet):
         super().__init__(frame, label, number=number, value=value, **kwargs)
         kwargs['text'] = dimensions
         self.dimensions = tk.Label(frame, **kwargs)
-        self.dimensions.grid(row=self.row, column=self.column + number + 1,
-                             padx=kwargs.pop('padx', self.PADX),
-                             pady=kwargs.pop('pady', self.PADY))
+        # self.dimensions.grid(row=self.row, column=self.column + number + 1,
+        #                      padx=kwargs.get('padx', self.PADX),
+        #                      pady=kwargs.get('pady', self.PADY))
+
+    def set_grid(self, row=None, column=None, **kwargs):
+        super().set_grid(row, column, **kwargs)
+        column += 1
+        self.dimensions.grid(row=row, column=column,
+                             padx=kwargs.get('padx', self.PADX),
+                             pady=kwargs.get('pady', self.PADY), **kwargs)
 
 
 class MultiCheckButtonSet(WidgetSet):
@@ -89,8 +118,16 @@ class MultiCheckButtonSet(WidgetSet):
             check_button = \
                 tk.Checkbutton(frame, variable=check_var, onvalue=True,
                                offvalue=False, **kwargs)
-            check_button.grid(row=self.row, column=self.column + 1 + i,
-                              padx=self.padx, pady=self.pady)
+            # check_button.grid(row=self.row, column=self.column + 1 + i,
+            #                   padx=self.padx, pady=self.pady)
             if value is not None and value[i] is True:
                 check_button.select()
             self.check_buttons.append(check_button)
+
+    def set_grid(self, row=None, column=None, **kwargs):
+        super().set_grid(row, column, **kwargs)
+        for i, check_button in enumerate(self.check_buttons):
+            column += 1
+            check_button.grid(row=row, column=column,
+                              padx=kwargs.get('padx', self.PADX),
+                              pady=kwargs.get('pady', self.PADY), **kwargs)
