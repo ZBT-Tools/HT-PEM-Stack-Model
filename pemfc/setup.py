d80d8ab3a3ff58bb387c06c63566942a9e52846d
lufire <lukas.feierabend@gmail.com>
2020-09-08 00:08:31 +0200

    added cx_freeze setup for non-anaconda python env

--- pemfc/setup.py
+++ pemfc/setup.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from cx_Freeze import setup, Executable
+import pkg_resources
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
@@ -13,26 +14,21 @@ PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
 os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
 os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
 icon_file = r'C:\Users\lukas\PycharmProjects\PEMFCModel\pemfc\logo-zbt.ico'
-build_exe_options = {'packages': ['numpy'],
-                     'includes': ['matplotlib.backends.backend_tkagg'],
-                     'include_files': [(os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tcl86t.dll'),
-                                        os.path.join('lib', 'tcl86t.dll')),
-                                       (os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tk86t.dll'),
-                                        os.path.join('lib', 'tk86t.dll'))
-                                       # add here further files which need to be included as described in 1.
-                                      ]}
+
+include_files = \
+    [(os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tcl86t.dll'),
+      os.path.join('lib', 'tcl86t.dll')),
+     (os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tk86t.dll'),
+      os.path.join('lib', 'tk86t.dll')), icon_file]
+
 packages = ["os", "numpy", "scipy", "matplotlib", "tkinter"]
 options = {
     'build_exe': {
         'packages': packages,
+        'namespace_packages': ['mpl_toolkits'],
         'includes': ['matplotlib.backends.backend_tkagg'],
-        'include_files': [(os.path.join(PYTHON_INSTALL_DIR, 'DLLs',
-                                        'tcl86t.dll'),
-                           os.path.join('lib', 'tcl86t.dll')),
-                          (os.path.join(PYTHON_INSTALL_DIR, 'DLLs',
-                                        'tk86t.dll'),
-                           os.path.join('lib', 'tk86t.dll')),
-                          icon_file]
+        'excludes': ['scipy.spatial', 'scipy.optimize'],
+        'include_files': include_files
                  }
           }
 setup(name="pemfc",
